b4468d8d61ede5b4a2b113536b730e754ac98c8d
Janus <jnus@live.com.sg>
2026-09-16 22:37:27 +0800

    feat: Add mew favicon

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,4 @@
-module.exports = {
-    tabWidth: 2,
-    singleQuote: true,
+export default {
+  tabWidth: 2,
+  singleQuote: true,
 };
--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,25 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
-    plugins: ["prettier"],
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs.flat['recommended-latest'],
       reactRefresh.configs.vite,
-      "<some-config>",
-      "plugin:prettier/recommended"
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    rules: {
-        "prettier/prettier": "warn"
-    },
   },
-])
+  prettierRecommended,
+]);
